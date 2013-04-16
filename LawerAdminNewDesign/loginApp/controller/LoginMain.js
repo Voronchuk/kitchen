e7281ad84d1b1188875016333c4b1d903404dc97
Irgola <irgola@mail.ru>
2013-04-16 16:15:49 +0300

    test

--- LawerAdminNewDesign/loginApp/controller/LoginMain.js
+++
@@ -1,8 +0,0 @@
-console.log("LoginApp.controller.LoginMain.js begins ....");
-Ext.define('LoginApp.controller.LoginMain', {
-    extend: 'Ext.app.Controller',
-    stores: [
-        'LoginApp.store.LoginStore'
-    ]
-});
-console.log("LoginApp.controller.LoginMain.js ends....");
